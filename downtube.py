9a9e98129d33c10cc126460be9156aef22284515
Javier García Cortés <garciacortesjavier@gmail.com>
2021-07-20 17:36:41 +0200

    Add files via upload
    Mostrar URLs videos de playlist

--- downtube.py
+++ downtube.py
@@ -1,20 +1,24 @@
 from pytube import YouTube
 from pytube import Playlist
 
-tipo = input('1. Video or 2. Playlist: ')
-url = input('URL: ')
-print('')
-
-if (tipo == '1'):
+def downvideo(url):
     yt = YouTube(url)
     print('Descargando: ', yt.title)
     stream = yt.streams.get_highest_resolution()
     stream.download('Descargas')
+
+
+tipo = input('1. Video or 2. Playlist: ')
+
+if (tipo == '1'):
+    url = input('URL video: ')
+    downvideo(url) 
     
-else:
+elif(tipo == '2'):
+    url = input('URL playlist: ')
     p = Playlist(url)
-    print('Descargando lista: ', p.title)
-    todos = input ('Todos (s/n)? ')
+    print('Lista: ', p.title)
+    todos = input ('Descargar todos los videos(s/n)? ')
     print('')
     
     if (todos == 's'):
@@ -22,11 +26,26 @@ else:
             print('Descargando: ', video.title)
             video.streams.get_highest_resolution().download(p.title)
             
-    else:
-        intervalo = input('Indica intervalo (numero video inicial y numero video final, separdos por espacio): ').split()
+    elif (todos == 'n'):
+        option = input('1. Obtener URLs videos or 2. Descargar intervalo de videos: ')
         print('')
-        
-        for i in range(int(intervalo[0]) - 1, int(intervalo[1])):
-            print('Descargando: ', p.videos[i].title)
-            p.videos[i].streams.get_highest_resolution().download(p.title)
+        if (option == '1'):
+            lista = []
+            for url in p.video_urls:
+                lista.append(url)
+
+            print(lista)
             
+        if (option == '2'):
+            intervalo = input('Indica intervalo (numero video inicial y numero video final, separdos por espacio): ').split()
+            print('')
+        
+            for i in range(int(intervalo[0]) - 1, int(intervalo[1])):
+                print('Descargando: ', p.videos[i].title)
+                p.videos[i].streams.get_highest_resolution().download(p.title)
+
+    else:
+        print('Selección erronea')
+
+else:
+     print('Selección erronea')
